71c5d0594fb7a04d2d2d161c38514ecfa8302922
Rakesh <rakesh.shaw018@gmail.com>
2023-02-15 02:28:53 +0530

    Linkquarium ver.1 created.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,21 +4,22 @@ from PIL import Image
 
 load_css()
 
-st.write("[![Star](https://img.shields.io/github/stars/dataprofessor/links.svg?logo=github&style=social)](https://gitHub.com/dataprofessor/links)")
+# st.write("[![Star](https://img.shields.io/github/stars/dataprofessor/links.svg?logo=github&style=social)](https://gitHub.com/dataprofessor/links)")
 
 col1, col2, col3 = st.columns(3)
 col2.image(Image.open('dp.png'))
 
-st.header('Chanin Nantasenamat, Ph.D.')
+st.header('Rakesh Shaw, Open-Source Enthusiast')
 
-st.info('Developer Advocate, Content Creator and ex-Professor with an interest in Data Science and Bioinformatics')
+st.info('👋Upcoming Dev👋, \n Success requires persistent practice, guided by patience.')
 
 icon_size = 20
 
-st_button('youtube', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel', icon_size)
-st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', icon_size)
-st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', icon_size)
-st_button('twitter', 'https://twitter.com/thedataprof/', 'Follow me on Twitter', icon_size)
-st_button('linkedin', 'https://www.linkedin.com/in/chanin-nantasenamat/', 'Follow me on LinkedIn', icon_size)
-st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', icon_size)
-st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
+st_button('resume','https://drive.google.com/file/d/1aBuysfNWHlq4BqacD-DVKsfDqglh6Drl/view?usp=share_link', 'View my Resume', icon_size)
+st_button('github', 'https://github.com/dev-rakeshshaw', 'Follow me on Github', icon_size)
+st_button('twitter', 'https://twitter.com/dev_rakeshshaw', 'Follow me on Twitter', icon_size)
+st_button('telegram', 'https://t.me/dev_rakeshshaw', 'Chat with me on Telegram', icon_size)
+st_button('linkedin', 'https://www.linkedin.com/in/rakesh-shaw-937514263/', 'Connect with me on LinkedIn', icon_size)
+
+
+
