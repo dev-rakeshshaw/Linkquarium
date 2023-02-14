ceb9e0565ab889f3e1a51b8bf2511f7776e1c312
Rakesh <rakesh.shaw018@gmail.com>
2023-02-15 03:01:44 +0530

    Linkquarium ver.1 modified.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,17 @@ import streamlit as st
 from st_functions import st_button, load_css
 from PIL import Image
 
+st.set_page_config(
+    page_title="Linkquarium",
+    page_icon="./images/favicon.PNG",
+)
+
 load_css()
 
 # st.write("[![Star](https://img.shields.io/github/stars/dataprofessor/links.svg?logo=github&style=social)](https://gitHub.com/dataprofessor/links)")
 
 col1, col2, col3 = st.columns(3)
-col2.image(Image.open('dp.png'))
+col2.image(Image.open('./images/dp.png'))
 
 st.header('Rakesh Shaw, Open-Source Enthusiast.')
 
